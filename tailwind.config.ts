674d65545d16cd8a631f0a42f566956026e988f3
xkpesc <xkpesc@proton.me>
2024-11-11 23:44:59 -0300

    added skeleton UI toolkit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,27 @@
-import typography from '@tailwindcss/typography';
+import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
-  theme: {
-    extend: {}
-  },
-
-  plugins: [typography]
+const config = {
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton
+	]
 } satisfies Config;
+
+export default config;
